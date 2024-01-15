3f0fe851c4a5d00c20e15e7b14f4d464afb81189
LucaCaligola <luca.caligola@gmail.com>
2024-01-15 12:22:21 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<!-- ## Snack 1
+
+Creiamo un array contenente le partite di basket di un'ipotetica tappa del calendario. 
+Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
+Stampiamo a schermo tutte le partite con questo schema:
+
+Olimpia Milano - Cantù | 55-60 -->
+
+
+
+<?php
+
+$array = partite (
+    {
+        "partita1" => {"squadraDiCasa"=> "Olimpia Milano",
+
+                        "squadrOspite"=> "Cantù",
+
+                        "puntiSquadraDiCasa"=> "55",
+
+                        "puntiSquadraOspite"=> "60"
+                        }
+
+                        "partita2" => {"squadraDiCasa"=> "Sirio",
+
+                            "squadrOspite"=> "Bellagio",
+    
+                            "puntiSquadraDiCasa"=> "45",
+    
+                            "puntiSquadraOspite"=> "53"
+                        }
+
+                        "partita2" => {"squadraDiCasa"=> "Sirio",
+
+                            "squadrOspite"=> "Bellagio",
+        
+                            "puntiSquadraDiCasa"=> "45",
+        
+                            "puntiSquadraOspite"=> "53"
+                        }
+    }
+
+)
+
+
+
+?>
